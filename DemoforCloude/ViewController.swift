a917ef74ea6330f1156f1a6620b471e29ca2d334
knoxpomacbookpro <himanshu.gandhi.knoxpo@gmail.com>
2021-01-07 15:21:46 +0530

    Initial Commit

---
+++ DemoforCloude/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoforCloude
+//
+//  Created by Knoxpo MacBook Pro on 07/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
